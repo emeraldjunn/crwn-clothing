b845d0bc00ea187e18a153c9ee8658360fe47687
unknown <emeraldjunn@hotmail.com>
2020-01-13 09:54:04 +0400

    First commit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../../component/directory/directory.component";
+const HomePage = () => (
+  <div className="hompage">
+    <Directory />
+  </div>
+);
+export default HomePage;
